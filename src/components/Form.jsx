eb72192508c6f5e3ed0ccef81f83023fbfdb844f
Marian0o1 <agustinsosa1733@hotmail.com>
2023-11-06 21:34:58 -0300

    First commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,39 @@
+export default function Form() {
+  return (
+    <div className=" center div-cotizador">
+      <h2 className="center separador">Completa los datos solicitados</h2>
+      <label htmlFor="propiedad">Selecciona el tipo de propiedad</label>
+      <select id="propiedad">
+        <option selected disabled>
+          ...
+        </option>
+      </select>
+      <label htmlFor="ubicacion">Selecciona su ubicación</label>
+      <select id="ubicacion">
+        <option selected disabled>
+          ...
+        </option>
+      </select>
+      <label htmlFor="metros2">Ingresa los Metros cuadrados:</label>
+      <input
+        type="number"
+        id="metros2"
+        value="20"
+        min="20"
+        max="500"
+        required
+      />
+      <div className="center separador">
+        <button className="button button-outline">Cotizar</button>
+      </div>
+      <div className="center separador">
+        <p className="importe">
+          Precio estimado: $ <span id="valorPoliza">0.00</span>
+          <span className="guardar ocultar" title="Guardar en historial">
+            💾
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <nav>
+      <div className="historial">
+          <span title="Ver Historial" id="botonEmoji">
+            📋
+          </span>
+      </div>
+      <h1 className="center separador">Seguros del hogar 🏡</h1>
+    </nav>
+  );
+}
